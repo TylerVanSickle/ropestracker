cb15d06a6f9b60430379cfca46c1772c23fee550
Tyler "Bubbles" VanSickle <126717933+TylerVanSickle@users.noreply.github.com>
2026-03-14 16:23:31 -0600

    Refactor notify message for clarity and formatting

--- src/app/lib/ropesMessage.js
+++ src/app/lib/ropesMessage.js
@@ -3,6 +3,9 @@
 export function buildNotifyMessage({ entry, estStartText }) {
   return `Hi there! You're up next for the ropes course.
 Please check in with the front desk!
-* If you do not check in with the front desk within 5 miuntes, your time will be forfeited to the next group in line.
-Estimated start: ${estStartText}`;
+
+Estimated start: ${estStartText}
+
+* Please check in at the front within 5 minutes, or your spot will go to the next in line.
+`;
 }
