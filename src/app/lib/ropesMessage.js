7b4b150fcfcdfe201325de444e2adf6a24ac559f
Tyler "Bubbles" VanSickle <126717933+TylerVanSickle@users.noreply.github.com>
2026-04-01 17:57:56 -0600

    Refactor buildNotifyMessage formatting
    Removed unnecessary line breaks in the notify message.

--- src/app/lib/ropesMessage.js
+++ src/app/lib/ropesMessage.js
@@ -6,6 +6,8 @@ Please check in with the front desk!
 
 Estimated start: ${estStartText}
 
-* Please check in at the front desk within 5 minutes to keep your spot. If you need more time, please call this number to adjust your start time.
+* Please check in at the front desk within 5 minutes to keep your spot.
+
+If you need more time, please call this number to adjust your start time.
 `;
 }
